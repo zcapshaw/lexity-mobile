3ded3581a5ee963aa38c6aefe0bf71220a0e7e95
Zach Capshaw <zcapshaw@gmail.com>
2020-06-24 12:34:53 -0600

    WIP - render both lists

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,25 +21,44 @@ class Book {
 
 class _HomeScreenState extends State<HomeScreen> {
   int wantToReadCount;
+  int readingCount;
 
+  Future<List<Book>> _getToReadList() async {
+    final String user = 'Users/77198';
+
+    var data = await http.get(
+        'https://stellar-aurora-280316.uc.r.appspot.com/list/summary/$user');
+    var toReadJson = jsonDecode(data.body)['TO_READ'];
+    List<Book> toReadList = [];
+
+    for (var b in toReadJson) {
+      Book book = Book(
+          title: b['title'], author: b['authors'][0], coverImage: b['cover']);
+      toReadList.add(book);
+    }
+    setState(() {
+      wantToReadCount = toReadList.length;
+    });
+    return toReadList;
+  }
+
+  //TODO: Refactor. This and above function are almost identical.
   Future<List<Book>> _getReadingList() async {
     final String user = 'Users/77198';
 
     var data = await http.get(
         'https://stellar-aurora-280316.uc.r.appspot.com/list/summary/$user');
-    var jsonData = jsonDecode(data.body)['TO_READ'];
+    var toReadJson = jsonDecode(data.body)['READING'];
     List<Book> readingList = [];
 
-    for (var b in jsonData) {
+    for (var b in toReadJson) {
       Book book = Book(
           title: b['title'], author: b['authors'][0], coverImage: b['cover']);
-
       readingList.add(book);
     }
     setState(() {
-      wantToReadCount = readingList.length;
+      readingCount = readingList.length;
     });
-
     return readingList;
   }
 
@@ -83,7 +102,7 @@ class _HomeScreenState extends State<HomeScreen> {
               Container(
                 margin: EdgeInsets.only(left: 20, top: 20, bottom: 10),
                 child: Text(
-                  'Want to read (${wantToReadCount ?? ''})',
+                  'Reading (${readingCount ?? ''})',
                   style: Theme.of(context).textTheme.headline6,
                 ),
               ),
@@ -91,6 +110,48 @@ class _HomeScreenState extends State<HomeScreen> {
               Container(
                 child: FutureBuilder(
                   future: _getReadingList(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.data == null) {
+                      return Container(
+                        child: Center(
+                          child: Text('Loading...'),
+                        ),
+                      );
+                    }
+
+                    return Flexible(
+                      child: ListView.separated(
+                          itemCount: snapshot.data.length,
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const Divider(),
+                          itemBuilder: (BuildContext context, int index) {
+                            return Column(
+                              children: <Widget>[
+                                ListTile(
+                                  leading: Image.network(
+                                      snapshot.data[index].coverImage),
+                                  title: Text(snapshot.data[index].title),
+                                  subtitle: Text(snapshot.data[index].author),
+                                  trailing: Icon(Icons.reorder),
+                                ),
+                              ],
+                            );
+                          }),
+                    );
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 20, top: 20, bottom: 10),
+                child: Text(
+                  'Want to read (${wantToReadCount ?? ''})',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+              Divider(),
+              Container(
+                child: FutureBuilder(
+                  future: _getToReadList(),
                   builder: (BuildContext context, AsyncSnapshot snapshot) {
                     if (snapshot.data == null) {
                       return Container(
