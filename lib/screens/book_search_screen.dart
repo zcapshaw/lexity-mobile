f6c872282d9b7a4a42f9fe235b24a631919211d4
Zach Capshaw <zcapshaw@gmail.com>
2020-07-11 13:22:09 -0600

    render a listview of book titles upon searching

--- lib/screens/book_search_screen.dart
+++ lib/screens/book_search_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -14,23 +14,49 @@ class _BookSearchScreenState extends State<BookSearchScreen> {
   final String illustration = 'assets/undraw_reading_time_gvg0.svg';
   String queryText = '';
 
-  void _fetchResults() async {
+  //This async function returns a List of BookTiles from the API response
+  Future<List<BookTile>> _fetchResults() async {
     final String user = 'Users/74763';
     final userJwt = DotEnv().env['USER_JWT'];
 
+    //fetch google books results based on queryText
     final http.Response data = await http.get(
         'https://stellar-aurora-280316.uc.r.appspot.com/search/private/books?userId=$user&searchText=$queryText',
         headers: {
           'user-jwt': '$userJwt',
         });
 
-    print(jsonDecode(data.body));
+    //declare an empty list of BookTiles
+    List<BookTile> books = [];
+
+    if (data.statusCode == 200) {
+      //decode JSON from API and store in a new variable
+      var jsonData = json.decode(data.body);
+
+      //Populate the books array by looping over jsonData and creating a BookTile for each element
+      for (var b in jsonData) {
+        //handle books that come back from Google with no cover data
+        if (b.containsKey('cover')) {
+          BookTile book = BookTile(b['title'], b['authors'[0]],
+              b['cover']['thumbnail'], b['inUserList']);
+          books.add(book);
+        } else {
+          BookTile book =
+              BookTile(b['title'], b['authors'[0]], null, b['inUserList']);
+          books.add(book);
+        }
+      }
+
+      print(books.length);
+    } else {
+      print(data.statusCode);
+      print(data.reasonPhrase);
+    }
+    return books;
   }
 
   @override
   Widget build(BuildContext context) {
-    _fetchResults();
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
@@ -81,7 +107,25 @@ class _BookSearchScreenState extends State<BookSearchScreen> {
                   ),
                 ),
               ),
-              AddBookBackground(),
+              Expanded(
+                child: FutureBuilder(
+                  future: _fetchResults(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.data == null || queryText == '') {
+                      return AddBookBackground();
+                    }
+                    return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        print(snapshot.data);
+                        return ListTile(
+                          title: Text(snapshot.data[index].title),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
@@ -116,4 +160,13 @@ class AddBookBackground extends StatelessWidget {
       ],
     );
   }
+}
+
+class BookTile {
+  final String title;
+  final String author;
+  final String thumbnail;
+  final bool inUserList;
+
+  BookTile(this.title, this.author, this.thumbnail, this.inUserList);
 }
