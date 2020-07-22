76cfbd05ca1f66a68fb574723da210f0cd48fedc
dmrediger <dmrediger@gmail.com>
2020-07-22 13:00:47 -0500

    Fixes to push bottom decor to last position in the stack, and adding text styling to Theme

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,111 +13,108 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.fromLTRB(20, 80, 20, 0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                  padding: EdgeInsets.fromLTRB(70, 0, 70, 20),
-                  child: Image.asset('assets/undraw_book_lover.png')),
-              Container(
-                child: Text(
-                  'Welcome to Lexity',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
+        bottom: false,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              bottom: 0,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset('assets/signin_bottom.png'),
               ),
-              Container(
-                padding: EdgeInsets.fromLTRB(0, 5, 0, 30),
-                child: Text(
-                  'Read great books. Share big ideas.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.grey[600],
-                    fontSize: 16,
-                    letterSpacing: 0.4,
-                    height: 1.5,
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 80, 20, 0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(70, 0, 70, 20),
+                    child: Image.asset('assets/undraw_book_lover.png'),
                   ),
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.75,
-                child: OutlineButton.icon(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  borderSide: BorderSide(color: Colors.grey[400]),
-                  label: Text(
-                    'SIGN UP WITH TWITTER',
-                    style: TextStyle(
-                      color: Colors.grey[600],
-                      letterSpacing: 0.3,
+                  Container(
+                    child: Text(
+                      'Welcome to Lexity',
+                      style: Theme.of(context).textTheme.headline4,
                     ),
                   ),
-                  icon: FaIcon(
-                    FontAwesomeIcons.twitter,
-                    color: Color(0xFF00ACEE),
-                  ),
-                  onPressed: () {
-                    print('Sign up for Twitter');
-                  },
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.75,
-                margin: EdgeInsets.only(top: 5),
-                child: OutlineButton.icon(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  borderSide: BorderSide(color: Colors.grey[400]),
-                  label: Text(
-                    'SIGN UP WITH APPLE',
-                    style: TextStyle(
-                      color: Colors.grey[600],
-                      letterSpacing: 0.3,
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 5, 0, 30),
+                    child: Text(
+                      'Read great books. Share big ideas.',
+                      style: Theme.of(context).textTheme.subtitle2,
                     ),
                   ),
-                  icon: FaIcon(
-                    FontAwesomeIcons.apple,
-                    color: Color(0xFF000000),
-                  ),
-                  onPressed: () {
-                    print('Sign up for Apple');
-                  },
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 20),
-                child: GestureDetector(
-                  onTap: () {
-                    print('Sign in');
-                  },
-                  child: RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                      text: 'Already have an account? ',
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 16,
-                        letterSpacing: 0.4,
-                        height: 1.5,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: 'Sign in',
-                          style:
-                              TextStyle(decoration: TextDecoration.underline),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.75,
+                    child: OutlineButton.icon(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      borderSide: BorderSide(color: Colors.grey[400]),
+                      label: Text(
+                        'SIGN UP WITH TWITTER',
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          letterSpacing: 0.3,
                         ),
-                      ],
+                      ),
+                      icon: FaIcon(
+                        FontAwesomeIcons.twitter,
+                        color: Color(0xFF00ACEE),
+                      ),
+                      onPressed: () {
+                        print('Sign up for Twitter');
+                      },
                     ),
                   ),
-                ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.75,
+                    margin: EdgeInsets.only(top: 5),
+                    child: OutlineButton.icon(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      borderSide: BorderSide(color: Colors.grey[400]),
+                      label: Text(
+                        'SIGN UP WITH APPLE',
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          letterSpacing: 0.3,
+                        ),
+                      ),
+                      icon: FaIcon(
+                        FontAwesomeIcons.apple,
+                        color: Color(0xFF000000),
+                      ),
+                      onPressed: () {
+                        print('Sign up for Apple');
+                      },
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 20),
+                    child: GestureDetector(
+                      onTap: () {
+                        print('Sign in');
+                      },
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          text: 'Already have an account? ',
+                          style: Theme.of(context).textTheme.subtitle2,
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Sign in',
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-      ),
-      bottomSheet: Container(
-        color: Colors.white,
-        child: Image.asset(
-          'assets/signin_bottom.png',
-          width: MediaQuery.of(context).size.width,
+            ),
+          ],
         ),
       ),
     );
