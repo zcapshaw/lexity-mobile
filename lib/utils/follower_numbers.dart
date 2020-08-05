14fc80c38a3479713e8d0fb3f35e75a78d6375c7
dmrediger <dmrediger@gmail.com>
2020-08-05 17:16:19 -0500

    Create FollowerNumbers class to confirm into thousands, millions, etc

---
+++ lib/utils/follower_numbers.dart
@@ -0,0 +1,33 @@
+class FollowerNumbers {
+  static String converter(String wholeNumber) {
+    int length = wholeNumber?.length;
+    String newNumber = '0';
+    if (length != null) {
+      if (length <= 3) {
+        newNumber = wholeNumber;
+      } else {
+        newNumber = addNotation(wholeNumber, length);
+      }
+    }
+    return newNumber;
+  }
+
+  static String addNotation(String largeNumber, int length) {
+    String notationNumber = '0';
+    double size = length / 3;
+    if (size <= 2) {
+      String base = largeNumber.substring(0, length - 3);
+      String decimal = largeNumber.substring(length - 3, length - 2);
+      notationNumber = base + '.' + decimal + 'K';
+    } else if (size <= 3) {
+      String base = largeNumber.substring(0, length - 6);
+      String decimal = largeNumber.substring(length - 6, length - 5);
+      notationNumber = base + '.' + decimal + 'M';
+    } else if (size <= 4) {
+      String base = largeNumber.substring(0, length - 9);
+      String decimal = largeNumber.substring(length - 9, length - 8);
+      notationNumber = base + '.' + decimal + 'B';
+    }
+    return notationNumber;
+  }
+}
