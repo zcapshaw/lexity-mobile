0a927bc9d8dab5082efb8f9d91b6c1693fbef8e0
Zach Capshaw <zcapshaw@gmail.com>
2020-08-06 07:07:18 -0600

    add an appbar for backward nav

--- lib/screens/book_detail_screen.dart
+++ lib/screens/book_detail_screen.dart
@@ -4,10 +4,16 @@ class BookDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Book Detail Screen',
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ),
         body: SafeArea(
-      child: Container(
-        child: Text('book detail screen'),
-      ),
-    ));
+          child: Container(
+            child: null,
+          ),
+        ));
   }
 }
