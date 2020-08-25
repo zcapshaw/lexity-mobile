2aea60fad7760c860cab178119f7339a7e3f2d57
dmrediger <dmrediger@gmail.com>
2020-08-24 22:10:17 -0500

    Update ReadinList to accomodate variations needed for READ books list on UserScreen and update the BookListBloc to hold a map of all list counts

--- lib/components/book_list_bloc.dart
+++ lib/components/book_list_bloc.dart
@@ -2,18 +2,31 @@ import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 
 class BookListBloc {
-  final BehaviorSubject<int> _readCountController = BehaviorSubject<int>();
+  final BehaviorSubject<Map<String, int>> _listCountController =
+      BehaviorSubject<Map<String, int>>();
 
-  Stream<int> get readCount => _readCountController.stream;
+  final Map<String, int> _listCountItems = {};
 
-  void updateReadCount(int count) {
-    if (!_readCountController.isClosed) {
-      _readCountController.sink.add(count);
+  Stream<Map<String, int>> get listCount => _listCountController.stream;
+
+  void addListCountItem(String type, int count) {
+    if (!_listCountController.isClosed) {
+      _listCountItems[type] = count;
+      _listCountController.sink.add(_listCountItems);
+    }
+  }
+
+  void removeListCountItem(String type) {
+    if (!_listCountController.isClosed) {
+      _listCountItems.keys
+          .where((k) => _listCountItems[k].toString() == type)
+          .forEach(_listCountItems.remove);
+      _listCountController.sink.add(_listCountItems);
     }
   }
 
   void dispose() {
-    _readCountController.close();
+    _listCountController.close();
   }
 }
 
