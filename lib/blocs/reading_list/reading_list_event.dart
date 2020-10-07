88d0646cbc7359adfa00b1cd40583770c2b081fb
dmrediger <dmrediger@gmail.com>
2020-10-06 20:04:30 -0500

    Add bloc events file

---
+++ lib/blocs/reading_list/reading_list_event.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+import 'package:lexity_mobile/models/models.dart';
+
+abstract class ReadingListEvent extends Equatable {
+  const ReadingListEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class ReadingListLoadSuccess extends ReadingListEvent {}
+
+class ReadingListRefreshed extends ReadingListEvent {}
+
+class ReadingListAdded extends ReadingListEvent {
+  final ListedBook book;
+
+  const ReadingListAdded(this.book);
+
+  @override
+  List<Object> get props => [book];
+
+  @override
+  String toString() => 'ReadingListAdded { book: $book }';
+}
+
+class ReadingListUpdated extends ReadingListEvent {
+  final ListedBook book;
+
+  const ReadingListUpdated(this.book);
+
+  @override
+  List<Object> get props => [book];
+
+  @override
+  String toString() => 'ReadingListUpdated { book: $book }';
+}
+
+class ReadingListDeleted extends ReadingListEvent {
+  final ListedBook book;
+
+  const ReadingListDeleted(this.book);
+
+  @override
+  List<Object> get props => [book];
+
+  @override
+  String toString() => 'ReadingListDeleted { book: $book }';
+}
