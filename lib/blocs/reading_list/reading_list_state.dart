cb8f102923efba08479a10f134e2fefb0ec6ca2b
dmrediger <dmrediger@gmail.com>
2020-10-06 19:40:44 -0500

    setup model barrel file and reading list state file

---
+++ lib/blocs/reading_list/reading_list_state.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:lexity_mobile/models/models.dart';
+
+abstract class ReadingListState extends Equatable {
+  const ReadingListState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class ReadingListLoadInProgress extends ReadingListState {}
+
+class ReadingListLoadSuccess extends ReadingListState {
+  final List<ListedBook> readingList;
+
+  const ReadingListLoadSuccess([this.readingList = const []]);
+
+  @override
+  List<Object> get props => [readingList];
+
+  @override
+  String toString() => 'ReadingListLoadSuccess { readingList: $readingList }';
+}
+
+class ReadingListLoadFailure extends ReadingListState {}
--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 export 'book.dart';
 export 'listed_book.dart';
 export 'note.dart';
 export 'user.dart';
+=======
+export './book.dart';
+export './listed_book.dart';
+export './note.dart';
+>>>>>>> setup model barrel file and reading list state file
