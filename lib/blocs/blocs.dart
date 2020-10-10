4cc996364febd849fcd4e919d89ad39a5bb155ef
dmrediger <dmrediger@gmail.com>
2020-10-10 14:51:41 -0500

    Integrate StatsCubit into MultiBlocProvider

--- lib/blocs/blocs.dart
+++ lib/blocs/blocs.dart
@@ -1,3 +1,4 @@
 export './authentication/bloc/authentication_bloc.dart';
 export './book_details/book_details_cubit.dart';
 export './reading_list/reading_list.dart';
+export './stats/stats_cubit.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,15 @@
+<<<<<<< HEAD
+=======
+import 'package:flutter/material.dart';
+import 'package:lexity_mobile/blocs/simple_bloc_observer.dart';
+import 'package:lexity_mobile/blocs/stats/stats_cubit.dart';
+import 'package:lexity_mobile/services/list_service.dart';
+import 'package:lexity_mobile/services/reading_list_service.dart';
+import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:get_it/get_it.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+>>>>>>> Integrate StatsCubit into MultiBlocProvider
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -48,9 +60,79 @@ class App extends StatelessWidget {
         BlocProvider<BookDetailsCubit>(
           create: (context) => BookDetailsCubit(),
         ),
+<<<<<<< HEAD
         BlocProvider<AuthenticationBloc>(
           create: (context) => AuthenticationBloc(
               authenticationRepository: authenticationRepository, userRepository: userRepository),
+=======
+        BlocProvider<StatsCubit>(
+          create: (context) => StatsCubit(
+            readingListBloc: BlocProvider.of<ReadingListBloc>(context),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        initialRoute: '/splash',
+        routes: {
+          '/': (context) => MainScreen(),
+          '/home': (context) => HomeScreen(),
+          '/user': (context) => UserScreen(),
+          '/login': (context) => LoginScreen(),
+          '/splash': (context) => SplashScreen(),
+          '/bookSearch': (context) => BookSearchScreen(),
+        },
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          appBarTheme: AppBarTheme(
+              color: Colors.grey[200],
+              brightness: Brightness.light,
+              iconTheme: IconThemeData(
+                color: Colors.grey[700],
+              )),
+          textTheme: TextTheme(
+            headline1: GoogleFonts.ibmPlexSerif(
+              fontWeight: FontWeight.w600,
+              color: Colors.grey[700],
+              fontSize: 24,
+            ),
+            headline3: GoogleFonts.ibmPlexSerif(
+              fontWeight: FontWeight.w600,
+              color: Colors.grey[700],
+            ),
+            headline4: GoogleFonts.ibmPlexSerif(
+              fontWeight: FontWeight.w600,
+              color: Color(0xFF1A6978),
+              fontSize: 36,
+            ),
+            headline6: GoogleFonts.roboto(
+              fontWeight: FontWeight.w700,
+              color: Colors.grey[700],
+              fontSize: 18,
+            ),
+            bodyText1: GoogleFonts.roboto(
+              color: Colors.grey[700],
+              fontWeight: FontWeight.w600,
+              letterSpacing: 0.75,
+            ),
+            bodyText2: GoogleFonts.roboto(
+              color: Colors.grey[700],
+            ),
+            caption: GoogleFonts.roboto(
+              color: Colors.grey[400],
+              fontSize: 12,
+            ),
+            subtitle1: GoogleFonts.roboto(
+              color: Colors.grey[700],
+            ),
+            subtitle2: GoogleFonts.roboto(
+              color: Colors.grey[600],
+              fontSize: 16,
+              letterSpacing: 0.4,
+              height: 1.5,
+            ),
+          ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+>>>>>>> Integrate StatsCubit into MultiBlocProvider
         ),
       ],
       child: AppView(),
