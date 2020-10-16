40839f87f1911f4696c68b49054c947e7e2642e6
dmrediger <dmrediger@gmail.com>
2020-10-16 06:55:36 -0500

    Update ReadingListEvents to include User class from AuthenticationBloc

--- lib/blocs/reading_list/reading_list_event.dart
+++ lib/blocs/reading_list/reading_list_event.dart
@@ -17,7 +17,7 @@ class ReadingListLoaded extends ReadingListEvent {
   List<Object> get props => [user];
 
   @override
-  String toString() => 'ReadingListLoaded { user: $user }';
+  String toString() => 'ReadingListLoaded { user: ${user.id} }';
 }
 
 class ReadingListRefreshed extends ReadingListEvent {
@@ -29,59 +29,66 @@ class ReadingListRefreshed extends ReadingListEvent {
   List<Object> get props => [user];
 
   @override
-  String toString() => 'ReadingListRefreshed { user: $user }';
+  String toString() => 'ReadingListRefreshed { user: ${user.id} }';
 }
 
 class ReadingListAdded extends ReadingListEvent {
-  const ReadingListAdded(this.book);
+  const ReadingListAdded(this.book, this.user);
 
   final ListedBook book;
+  final User user;
 
   @override
-  List<Object> get props => [book];
+  List<Object> get props => [book, user];
 
   @override
-  String toString() => 'ReadingListAdded { book: $book }';
+  String toString() =>
+      'ReadingListAdded { book: ${book.bookId}, user: ${user.id} }';
 }
 
 class ReadingListUpdated extends ReadingListEvent {
-  const ReadingListUpdated(this.updatedBook);
+  const ReadingListUpdated(this.updatedBook, this.user);
 
   final ListedBook updatedBook;
+  final User user;
 
   @override
-  List<Object> get props => [updatedBook];
+  List<Object> get props => [updatedBook, user];
 
   @override
-  String toString() => 'ReadingListUpdated { updatedBook: $updatedBook }';
+  String toString() =>
+      'ReadingListUpdated { updatedBook: ${updatedBook.bookId}, user: ${user.id} }';
 }
 
 class ReadingListReordered extends ReadingListEvent {
-  const ReadingListReordered(this.oldIndex, this.newIndex,
+  const ReadingListReordered(this.oldIndex, this.newIndex, this.user,
       {this.isHomescreen = false});
 
   final int oldIndex;
   final int newIndex;
+  final User user;
   final bool isHomescreen;
 
   @override
-  List<Object> get props => [oldIndex, newIndex];
+  List<Object> get props => [oldIndex, newIndex, user];
 
   @override
   String toString() =>
-      'ReadingListReordered { oldIndex: $oldIndex, newIndex: $newIndex, isHomescreen: $isHomescreen }';
+      'ReadingListReordered { oldIndex: $oldIndex, newIndex: $newIndex, user: ${user.id}, isHomescreen: $isHomescreen }';
 }
 
 class ReadingListDeleted extends ReadingListEvent {
-  const ReadingListDeleted(this.book);
+  const ReadingListDeleted(this.book, this.user);
 
   final ListedBook book;
+  final User user;
 
   @override
-  List<Object> get props => [book];
+  List<Object> get props => [book, user];
 
   @override
-  String toString() => 'ReadingListDeleted { book: $book }';
+  String toString() =>
+      'ReadingListDeleted { book: ${book.bookId}, user: ${user.id} }';
 }
 
 class ReadingListDismount extends ReadingListEvent {}
