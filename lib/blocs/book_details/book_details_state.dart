369d7e05c3e725736eac662aaf2efacb7e1e3575
dmrediger <dmrediger@gmail.com>
2021-01-04 20:11:58 -0700

    Add ListedBook book to equality testing for BookDetailsState to ensure that when swiping back from BookDetailScreen and clicking a different book, that different book will be displayed appropriately

--- lib/blocs/book_details/book_details_state.dart
+++ lib/blocs/book_details/book_details_state.dart
@@ -17,21 +17,33 @@ class BookDetailsLoading extends BookDetailsState {
 
 //The following 4 states allow the screen to conditionally show correct action buttons
 class BookDetailsReading extends BookDetailsState {
-  final ListedBook book;
   const BookDetailsReading(this.book);
+  final ListedBook book;
+
+  @override
+  List<Object> get props => [book];
 }
 
 class BookDetailsWantToRead extends BookDetailsState {
-  final ListedBook book;
   const BookDetailsWantToRead(this.book);
+  final ListedBook book;
+
+  @override
+  List<Object> get props => [book];
 }
 
 class BookDetailsFinished extends BookDetailsState {
-  final ListedBook book;
   const BookDetailsFinished(this.book);
+  final ListedBook book;
+
+  @override
+  List<Object> get props => [book];
 }
 
 class BookDetailsUnlisted extends BookDetailsState {
-  final ListedBook book;
   const BookDetailsUnlisted(this.book);
+  final ListedBook book;
+
+  @override
+  List<Object> get props => [book];
 }
