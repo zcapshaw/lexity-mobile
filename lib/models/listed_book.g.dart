23a8ccf1490eec9d87dfbb781a4fbc01301f57f3
dmrediger <dmrediger@gmail.com>
2021-01-26 22:17:26 -0700

    Update the note model to accomodate new twitter sourced variables

--- lib/models/listed_book.g.dart
+++ lib/models/listed_book.g.dart
@@ -38,6 +38,16 @@ ListedBook _$ListedBookFromJson(Map<String, dynamic> json) {
 Map<String, dynamic> _$ListedBookToJson(ListedBook instance) {
   final val = <String, dynamic>{
     'title': instance.title,
+    'subtitle': instance.subtitle,
+    'authors': instance.authors,
+    'googleId': instance.googleId,
+    'description': instance.description,
+    'categories': instance.categories,
+    'listId': instance.listId,
+    'type': instance.type,
+    'recos': instance.recos,
+    'inUserList': instance.inUserList,
+    'userRead': instance.userRead,
   };
 
   void writeNotNull(String key, dynamic value) {
@@ -46,21 +56,11 @@ Map<String, dynamic> _$ListedBookToJson(ListedBook instance) {
     }
   }
 
-  writeNotNull('subtitle', instance.subtitle);
-  writeNotNull('authors', instance.authors);
   writeNotNull('cover', instance.cover);
-  writeNotNull('googleId', instance.googleId);
-  writeNotNull('description', instance.description);
-  writeNotNull('categories', instance.categories);
-  writeNotNull('listId', instance.listId);
   writeNotNull('userId', instance.userId);
   writeNotNull('bookId', instance.bookId);
-  writeNotNull('inUserList', instance.inUserList);
-  writeNotNull('userRead', instance.userRead);
   writeNotNull('created', instance.created);
   writeNotNull('updated', instance.updated);
-  writeNotNull('type', instance.type);
-  writeNotNull('recos', instance.recos);
   writeNotNull('labels', instance.labels);
   writeNotNull('notes', instance.notes);
   return val;
