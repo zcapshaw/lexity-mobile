0544af66856ad49de879eaafc2326fc25feffbeb
dmrediger <dmrediger@gmail.com>
2021-01-26 22:19:37 -0700

    Update AddBook and AddReco screens to leverage twitter user data to show a list of followed users in recoSource type-ahead

--- lib/screens/add_reco_screen.dart
+++ lib/screens/add_reco_screen.dart
@@ -11,12 +11,22 @@ import '../services/services.dart';
 import '../utils/utils.dart';
 
 class AddRecoScreen extends StatefulWidget {
-  const AddRecoScreen({Key key, this.recoSource, this.recoText, this.userId})
+  const AddRecoScreen(
+      {Key key,
+      this.recoSource,
+      this.recoText,
+      this.recoTwitterScreenName,
+      this.sourceImg,
+      this.sourceTwitterId,
+      this.sourceTwitterVerified})
       : super(key: key);
 
   final String recoSource;
   final String recoText;
-  final String userId; // To be used with lexity and/or twitter search
+  final String recoTwitterScreenName;
+  final String sourceImg;
+  final int sourceTwitterId;
+  final bool sourceTwitterVerified;
 
   @override
   _AddRecoScreenState createState() => _AddRecoScreenState();
@@ -25,16 +35,30 @@ class AddRecoScreen extends StatefulWidget {
 class _AddRecoScreenState extends State<AddRecoScreen> {
   String recoSource;
   String recoText;
+  String recoTwitterScreenName;
+  String sourceImg;
+  int sourceTwitterId;
+  bool sourceTwitterVerified;
   bool isConnected;
   ListService listService = ListService();
   User user;
   final double twitterImgSize = 50.0;
   Timer _debounce;
+  FocusNode recoSourceFocus = FocusNode();
+  TextEditingController recoSourceTxtController;
+  TextEditingController recoTextTxtController;
 
   @override
   void initState() {
     recoSource = widget.recoSource;
     recoText = widget.recoText;
+    recoTwitterScreenName = widget.recoTwitterScreenName;
+    sourceImg = widget.sourceImg;
+    sourceTwitterId = widget.sourceTwitterId;
+    sourceTwitterVerified = widget.sourceTwitterVerified;
+    recoSourceTxtController = TextEditingController(text: widget.recoSource);
+    recoTextTxtController = TextEditingController(text: widget.recoText);
+    recoSourceFocus.addListener(_onRecoSourceFocusChange);
     user = context.bloc<AuthenticationBloc>().state.user;
     _isConnected();
     super.initState();
@@ -43,6 +67,7 @@ class _AddRecoScreenState extends State<AddRecoScreen> {
   @override
   void dispose() {
     _debounce?.cancel();
+    recoSourceFocus.dispose();
     super.dispose();
   }
 
@@ -51,15 +76,65 @@ class _AddRecoScreenState extends State<AddRecoScreen> {
     isConnected = await InternetConnectionTest.isConnected();
   }
 
-  Future<List> _twitterUserList() async {
+  // called by onTextChange of reco input
+  void _debounceRecoSource(String text) {
     if (_debounce?.isActive ?? false) _debounce.cancel();
-    _debounce = Timer(const Duration(milliseconds: 500), () async {
-      print('isConnected:$isConnected');
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      setState(() {
+        recoSource = text;
+      });
+    });
+  }
+
+  void _onTapTwitterUser(
+      {int twitterId,
+      String twitterName,
+      String twitterScreenName,
+      String twitterImg,
+      bool twitterVerified}) {
+    recoSourceFocus.unfocus();
+    recoSourceTxtController.text = twitterName;
+    setState(() {
+      sourceTwitterId = twitterId;
+      recoSource = twitterName;
+      recoTwitterScreenName = twitterScreenName;
+      sourceImg = twitterImg;
+      sourceTwitterVerified = twitterVerified;
+    });
+  }
+
+  Future<List> _twitterUserList() async {
+    print('isConnected:$isConnected');
+    if (recoSourceFocus.hasFocus) {
       final twitterUsers = await listService.searchTwitterUsers(
           user.accessToken, user.id, recoSource);
       final decoded = jsonDecode(twitterUsers.data as String) as List;
       print(decoded);
       return decoded;
+    } else {
+      return <String>[];
+    }
+  }
+
+  void _onRecoSourceFocusChange() {
+    if (recoSourceFocus.hasFocus) {
+      // By calling setState, the Futurebuilder is called and will build w/
+      // recoSourceFocus.hasFocus == true, which will trigger _twitterUserList
+      // and query the backend
+      setState(() {});
+    }
+  }
+
+  void _onRecoSourceSubmitted(String text) {
+    // If a user clicks in the recoSource text field and instead of choosing a
+    // Twitter user (if available), they press the done button the keyboard,
+    // this is then treated as pure text input and all twitter association
+    // is removed
+    setState(() {
+      recoTwitterScreenName = null;
+      sourceImg = null;
+      sourceTwitterId = null;
+      sourceTwitterVerified = null;
     });
   }
 
@@ -78,8 +153,14 @@ class _AddRecoScreenState extends State<AddRecoScreen> {
                   ? Scaffold.of(context).showSnackBar(SnackBar(
                       backgroundColor: Colors.red[300],
                       content: const Text('Reco notes require a source.')))
-                  : Navigator.pop(context,
-                      {'recoSource': recoSource, 'recoText': recoText});
+                  : Navigator.pop(context, {
+                      'recoSource': recoSource,
+                      'recoText': recoText,
+                      'sourceTwitterId': sourceTwitterId,
+                      'recoTwitterScreenName': recoTwitterScreenName,
+                      'sourceImg': sourceImg,
+                      'sourceTwitterVerified': sourceTwitterVerified
+                    });
             },
             child: Text(
               'Done',
@@ -97,15 +178,15 @@ class _AddRecoScreenState extends State<AddRecoScreen> {
           child: Column(
             children: <Widget>[
               TextFieldTile(
-                intialValue: recoSource,
+                focusNode: recoSourceFocus,
+                controller: recoSourceTxtController,
                 headerText: 'Who recommended this book to you?',
                 hintText: 'Type a name',
                 maxLines: 1,
                 onTextChange: (text) {
-                  setState(() {
-                    recoSource = text;
-                  });
+                  _debounceRecoSource(text);
                 },
+                onSubmitted: _onRecoSourceSubmitted,
               ),
               const Divider(),
               Expanded(
@@ -113,57 +194,84 @@ class _AddRecoScreenState extends State<AddRecoScreen> {
                     future: _twitterUserList(),
                     builder:
                         (BuildContext context, AsyncSnapshot<List> snapshot) {
-                      print('snapshot data ${snapshot.data}');
-                      if (snapshot.data == null ||
-                          snapshot.data.isEmpty ||
-                          recoSource.isEmpty) {
-                        return TextFieldTile(
-                          intialValue: recoText,
-                          headerText: 'Notes',
-                          hintText: 'Add a note about what they said...',
-                          maxLines: null,
-                          onTextChange: (text) {
-                            setState(() {
-                              recoText = text;
-                            });
-                          },
-                        );
-                      }
-                      return ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Column(
-                            children: <Widget>[
-                              ListTile(
-                                title: Text(
-                                    snapshot.data[index]['name'].toString()),
-                                subtitle: Text(
-                                    '@${snapshot.data[index]['screen_name'].toString()}'),
-                                leading: Container(
-                                  width: twitterImgSize,
-                                  height: twitterImgSize,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: NetworkImage(snapshot.data[index]
-                                                  ['profile_image_url_https']
-                                              .toString() ??
-                                          ''),
+                      print('snapshot data: ${snapshot.data}');
+                      print('Focused? ${recoSourceFocus.hasFocus}');
+                      if (recoSourceFocus.hasFocus) {
+                        if (snapshot.data == null ||
+                            snapshot.data.isEmpty ||
+                            recoSource.isEmpty) {
+                          return buildRecoTextInput();
+                        } else {
+                          return ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Column(
+                                children: <Widget>[
+                                  ListTile(
+                                    title: Text(snapshot.data[index]['name']
+                                        .toString()),
+                                    subtitle: Text(
+                                        // ignore: lines_longer_than_80_chars
+                                        '@${snapshot.data[index]['screen_name'].toString()}'),
+                                    leading: Container(
+                                      width: twitterImgSize,
+                                      height: twitterImgSize,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(snapshot
+                                                  .data[index][
+                                                      // ignore: lines_longer_than_80_chars
+                                                      'profile_image_url_https']
+                                                  .toString() ??
+                                              ''),
+                                        ),
+                                      ),
                                     ),
+                                    onTap: () => _onTapTwitterUser(
+                                        twitterId:
+                                            snapshot.data[index]['id'] as int,
+                                        twitterName: snapshot.data[index]
+                                            ['name'] as String,
+                                        twitterScreenName: snapshot.data[index]
+                                            ['screen_name'] as String,
+                                        twitterImg: snapshot.data[index]
+                                                ['profile_image_url_https']
+                                            as String,
+                                        twitterVerified: snapshot.data[index]
+                                            ['verified'] as bool),
                                   ),
-                                ),
-                              )
-                            ],
+                                  const Divider(),
+                                ],
+                              );
+                            },
                           );
-                        },
-                      );
+                        }
+                      } else {
+                        // if !recoSourceFocus.hasFocus
+                        return buildRecoTextInput();
+                      }
                     }),
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildRecoTextInput() {
+    return TextFieldTile(
+      controller: recoTextTxtController,
+      headerText: 'Notes',
+      hintText: 'Add a note about what they said...',
+      maxLines: null,
+      onTextChange: (text) {
+        setState(() {
+          recoText = text;
+        });
+      },
     );
   }
 }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,3 @@
 export './follower_numbers.dart';
+export './internet_connection_test.dart';
 export './test_keys.dart';
-export 'internet_connection_test.dart';
