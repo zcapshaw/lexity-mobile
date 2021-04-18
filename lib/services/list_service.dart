595d14019c49e8c905bed32bf13d5eab76e84562
dmrediger <dmrediger@gmail.com>
2021-04-18 12:42:57 -0600

    Integrate updateUser API call and leverage it for syncing first and last node of the doubly linked list

--- lib/services/list_service.dart
+++ lib/services/list_service.dart
@@ -170,6 +170,31 @@ class ListService {
           error: true, errorMessage: 'An error occured'),
     );
   }
+
+  Future<APIResponse> updateUser(User user, Map userUpdate) {
+    final jsonItem = jsonEncode({'userId': user.id, 'userUpdate': userUpdate});
+    return http
+        .post(
+      '$API/user/update',
+      headers: {
+        'access-token': user.accessToken,
+        'Content-Type': 'application/json',
+      },
+      body: jsonItem,
+    )
+        .then((res) {
+      if (res.statusCode == 200) {
+        return const APIResponse<bool>(data: true);
+      }
+      return APIResponse<bool>(
+          error: true,
+          errorCode: res.statusCode,
+          errorMessage: res.reasonPhrase);
+    }).catchError(
+      (dynamic err) => const APIResponse<bool>(
+          error: true, errorMessage: 'An error occured'),
+    );
+  }
 }
 
 class APIResponse<T> {
