6a9e3d6f6ae6b994e92add535d222e4a39d78d1d
dmrediger <dmrediger@gmail.com>
2021-04-28 11:19:54 -0600

    Move twitter user search API from ListService to TwitterService and update AddRecoScreen to leverage the TwitterService

--- lib/services/twitter_service.dart
+++ lib/services/twitter_service.dart
@@ -6,6 +6,25 @@ class TwitterService {
   // ignore: constant_identifier_names
   static const API = 'https://api.lexity.co';
 
+  Future<APIResponse<Object>> searchTwitterUsers(
+      String accessToken, String userId, String query) async {
+    return http.get(
+      '$API/twitter/search/user?userId=$userId&q=$query',
+      headers: {'access-token': accessToken},
+    ).then((res) {
+      if (res.statusCode == 200) {
+        return APIResponse<Object>(data: res.body);
+      }
+      return APIResponse<Object>(
+          error: true,
+          errorCode: res.statusCode,
+          errorMessage: res.reasonPhrase);
+    }).catchError(
+      (dynamic err) => const APIResponse<Object>(
+          error: true, errorMessage: 'An error occured'),
+    );
+  }
+
   Future<APIResponse> shareTwitterNotes(
       String accessToken, String userId, List<String> notes) {
     final jsonPayload = jsonEncode({
