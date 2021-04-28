9ef774bcb2ca57680f6338cc6c6459d1e62a0fb5
dmrediger <dmrediger@gmail.com>
2021-04-28 11:08:16 -0600

    Update TwitterService to use API class extracted to utils

--- lib/services/twitter_service.dart
+++ lib/services/twitter_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'list_service.dart';
+import 'package:lexity_mobile/utils/utils.dart';
 
 class TwitterService {
   // ignore: constant_identifier_names
