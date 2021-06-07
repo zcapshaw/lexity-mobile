a2a94dc2a1fc8da18b5600c77a55ecc85294c1ac
dmrediger <dmrediger@gmail.com>
2021-06-07 13:49:10 -0600

    Update Google to be bold and use a font similar to the Google logo

--- lib/screens/book_search_screen.dart
+++ lib/screens/book_search_screen.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:lexity_mobile/blocs/authentication/bloc/authentication_bloc.dart';
@@ -238,17 +239,30 @@ class AddBookBackground extends StatelessWidget {
         ),
         Container(
           padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 80),
-          child: Container(
-            child: Text(
-              'Powered by Google',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.grey[500],
-                fontSize: 14,
-                letterSpacing: 0.4,
-                height: 1.5,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Powered by ',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                  fontSize: 14,
+                  letterSpacing: 0.4,
+                  height: 1.5,
+                ),
               ),
-            ),
+              Text(
+                'Google',
+                // Style recommended to align with the unavailable Public Sans
+                style: GoogleFonts.nunito(
+                  color: Colors.grey[500],
+                  fontWeight: FontWeight.w800,
+                  fontSize: 14,
+                  letterSpacing: 0.4,
+                  height: 1.5,
+                ),
+              ),
+            ],
           ),
         ),
       ],
