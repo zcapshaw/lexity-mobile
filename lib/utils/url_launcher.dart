721de330b7728d4158d0856811249d6004d56f51
dmrediger <dmrediger@gmail.com>
2021-06-08 09:00:25 -0600

    Update url_launcher to have platform conditionals within the util and only require passing the URL string - update this in the authentication_repo where it is currently used

--- lib/utils/url_launcher.dart
+++ lib/utils/url_launcher.dart
@@ -1,16 +1,21 @@
+import 'dart:io';
+
 import 'package:url_launcher/url_launcher.dart';
 
 class UrlLauncher {
-  Future<void> launchInWebViewOrVC(
-      String url, bool forceSafariVC, bool forceWebView) async {
-    if (await canLaunch(url)) {
-      await launch(
-        url,
-        forceSafariVC: forceSafariVC,
-        forceWebView: forceWebView,
-      );
+  Future<void> launchInWebViewOrVC(String url) async {
+    if (Platform.isIOS || Platform.isAndroid) {
+      if (await canLaunch(url)) {
+        await launch(
+          url,
+          forceSafariVC: Platform.isIOS ? true : false,
+          forceWebView: Platform.isIOS ? true : false,
+        );
+      } else {
+        throw Exception('Could not launch $url');
+      }
     } else {
-      throw Exception('Could not launch $url');
+      throw Exception('Warning: Platform is NOT iOS or Android');
     }
   }
 }
